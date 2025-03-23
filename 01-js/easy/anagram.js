8809123a21b11eeff1490296acb385e701decbce
Agastya Khati <krishkhati70@gmail.com>
2025-03-23 16:06:45 +0530

    anagram

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,19 @@
 */
 
 function isAnagram(str1, str2) {
-
+  let str="";
+  for(let i = 0;i<str1.length;i++){
+    for(let j = 0;j<str2.length;j++){
+      if(str1[i]==str2[j]){
+        str += str[i];
+      }
+    }
+  }
 }
+  if(str == str2){
+    return true;
+  } else {
+    return false
+  }
 
 module.exports = isAnagram;
